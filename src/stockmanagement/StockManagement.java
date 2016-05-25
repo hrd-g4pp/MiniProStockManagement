453b753ee9a6f1aa3ebb1350c092162a0f742ca1
mineachem <minea.chem@gmail.com>
2016-05-25 06:30:02 -0700

    hello world

--- src/stockmanagement/StockManagement.java
+++ src/stockmanagement/StockManagement.java
@@ -133,6 +133,6 @@ public class StockManagement {
         s.writeRecord();
         s.readRecord();
         
-        System.out.println("testing....coding");
+        System.out.println("testing....coding with github");
     }
 }
